32c45ea8b0b32021d6477cb559314d6653ef6d9f
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-22 15:37:19 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,86 +1,24 @@
-# book.rb
-# Add you Book class here
-class Book
-  def initilize(title)
-    @title = title
-  end
-
-  def title
-    @title
-  end
-
-  def author= (author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_number= (pages)
-    @page_number = pages
-  end
-
-  def page_number
-    @page_count
-  end
-
-  def genre= (genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
-
-  def turn_page
-    puts "Flipping the page .....wow, you read fast!"
-  end
-end
-
-Harry_Potter = Book.new("Harry Potter and the Sorcerer's stone")
-Harry_Potter.turn_page
-
-
-
-
-
 
 #copied one
 
 
 class Book
-  attr_accesor :author :page_count :genre
+  attr_accesor :author, :page_count, :genre
   def initialize(title)
     @title = title
   end
 attr_reader :title
-  def title
-    @title
-  end
 
   def author=(author)
     @author = author
-  end
-
-  def author
-    @author
   end
 
   def page_count=(num)
     @page_count = num
   end
 
-  def page_count
-    @page_count
-  end
-
   def genre=(genre)
     @genre = genre
-  end
-
-  def genre
-    @genre
   end
 
   def turn_page
