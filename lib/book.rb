20f6230ddc5098f638f6ada27d00f41752ae83af
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-22 15:25:12 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -41,3 +41,44 @@ end
 Harry_Potter = Book.new("Harry Potter and the Sorcerer's stone")
 Harry_Potter.turn_page
 
+
+
+
+class Book
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+  end
+
+end
