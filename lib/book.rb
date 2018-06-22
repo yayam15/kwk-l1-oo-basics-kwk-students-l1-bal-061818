6f6bacb1a18dfd5f9e68b6d6df2b09b1e80dd519
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-22 15:23:21 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -19,7 +19,44 @@ class Book
   def page_count= (pages)
     @page_count = pages
   end
-
+  class Book
+   
+    def initialize(title)
+      @title = title
+    end
+   
+    def title
+      @title
+    end
+   
+    def author=(author)
+      @author = author
+    end
+   
+    def author
+      @author
+    end
+   
+    def page_count=(num)
+      @page_count = num
+    end
+   
+    def page_count
+      @page_count
+    end
+   
+    def genre=(genre)
+      @genre = genre
+    end
+   
+    def genre
+      @genre
+    end
+   
+    def turn_page
+    end
+   
+  end
   def page_count
     @page_count
   end
